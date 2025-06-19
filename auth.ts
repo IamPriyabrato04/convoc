1e84f387138344e5098178c89ad4f8afa5fd937b
Priyabrato <sahpriyabrato@gmail.com>
2025-06-19 23:46:58 +0530

    Add protected pages and layout: implement Previous, Upcoming, Recordings, and Personal Room pages; create Dashboard layout and sidebar links; update authentication event for email verification

--- auth.ts
+++ auth.ts
@@ -4,6 +4,14 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    events: {
+        linkAccount: async ({ user }) => {
+            await db.user.update({
+                where: { id: user.id },
+                data: { emailVerified: new Date() }, // Set email verified date on account link
+            })
+        }
+    },
     callbacks: {
         async session({ session, token }) {
             if (token.sub && session.user) {
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        imgUrl: '',
+        route: '/dashboard',
+    },
+    {
+        label: 'Upcoming',
+        imgUrl: '',
+        route: '/upcoming',
+    },
+    {
+        label: 'Previous',
+        imgUrl: '',
+        route: '/previous',
+    },
+    {
+        label: 'Recordings',
+        imgUrl: '',
+        route: '/recordings',
+    },
+    {
+        label: 'Personal Room',
+        imgUrl: '',
+        route: '/personal-room',
+    }
+]
